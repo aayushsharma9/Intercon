a722e07d79f603856ff8f0f4bbc9227812cbd67b
aayushsharma9 <aayush.sharma317@gmail.com>
2019-02-18 19:51:43 +0530

    Tweaked portals

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -4,37 +4,27 @@ using UnityEngine;
 
 public class Teleport : MonoBehaviour
 {
-    public GameObject tele1, tele2, Player;
+    public GameObject connectingTele;
+    public static bool isTeleporting;
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {        
-        if(other.tag == "Tele")
-        {     
-            StartCoroutine(TeleportWait());
+    private void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            if (!isTeleporting)
+            {
+                isTeleporting = true;
+                Vector2 connectingTelePosition = new Vector2 (connectingTele.transform.position.x, connectingTele.transform.position.y);
+                other.transform.position = connectingTelePosition;
+            }
         }
     }
 
-    IEnumerator TeleportWait()
+    private void OnTriggerExit2D (Collider2D other)
     {
-        yield return new WaitForSeconds(0.8f);
-        
-        Vector2 teleD1 = new Vector2(tele1.transform.position.x, tele1.transform.position.y);
-        Vector2 teleD2 = new Vector2(tele2.transform.position.x, tele2.transform.position.y);
-        Vector2 PlayerD = new Vector2(Player.transform.position.x, Player.transform.position.y);
-
-        if(Vector2.Distance(teleD1,PlayerD) <= 0.9)          
-            {
-                Debug.Log("TELE1 holo frandzz");
-                Player.transform.position = new Vector2(tele2.transform.position.x, tele2.transform.position.y);
-            }  
-              
-            else
-            {
-                if(Vector2.Distance(teleD2,PlayerD) <= 0.9)
-                {   
-                    Debug.Log("TELE2 holo frandzz");
-                    Player.transform.position = new Vector2(tele1.transform.position.x, tele1.transform.position.y);
-                }
-            }
+        if (other.tag == "Player")
+        {
+            isTeleporting = false;
+        }
     }
 }
