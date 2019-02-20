e52dbc0952bc294e14ee2ac32574b1b71ca1210b
Sterbon <talwar.utsav@gmail.com>
2019-02-20 22:28:07 +0530

    Platform Explosion

---
+++ Assets/Scripts/PlatformDestroy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformDestroy : MonoBehaviour
+{
+    public int destroySec;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Wait for it!");
+            StartCoroutine(destroy(destroySec));
+        }
+    }
+    IEnumerator destroy(int destroySec)
+    {
+        yield return new WaitForSeconds(destroySec);
+        Destroy(gameObject);
+        Debug.Log("Ayy Boom!!");
+    }
+
+}
