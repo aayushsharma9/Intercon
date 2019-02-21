2098a34504797aba38ea252e5f2e0632480ae63e
aayushsharma9 <aayush.sharma317@gmail.com>
2019-02-21 14:20:40 +0530

    More polish

--- Assets/Scripts/PlatformDestroy.cs
+++ Assets/Scripts/PlatformDestroy.cs
@@ -4,21 +4,15 @@ using UnityEngine;
 
 public class PlatformDestroy : MonoBehaviour
 {
-    public int destroySec;
+    public AnimationClip destroyClip;
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter2D (Collision2D collision)
     {
-        if(collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player")
         {
-            Debug.Log("Wait for it!");
-            StartCoroutine(destroy(destroySec));
+            gameObject.GetComponentInChildren<Animator> ().Play ("Autodestroy");
+            gameObject.GetComponentInChildren<BoxCollider2D> ().enabled = false;
+            Destroy (gameObject, destroyClip.length);
         }
     }
-    IEnumerator destroy(int destroySec)
-    {
-        yield return new WaitForSeconds(destroySec);
-        Destroy(gameObject);
-        Debug.Log("Ayy Boom!!");
-    }
-
 }
